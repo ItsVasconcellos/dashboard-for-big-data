96d4dd778dac5c814781a0fc8d0ec7a8eaf7f96c
ItsVasconcellos <fernando.asv@uol.com.br>
2025-12-01 11:21:20 +0000

    feat: final version of csv script

--- src/data/extract_data_csv_to_sql.py
+++ src/data/extract_data_csv_to_sql.py
@@ -29,7 +29,7 @@ with open(csv_file, encoding="utf-8") as f:
         manufacturer_model_key = f'{manufacturer_name}|{row["Model"]}|{row['Engine size']}'
         if manufacturer_model_key not in manufacturer_models:
             new_manufacturer_model = {
-                "manucaturer_model_id": uuid.uuid4(),
+                "manufacturer_model_id": uuid.uuid4(),
                 "model_name": row["Model"],
                 "engine_size": row["Engine size"],
                 "fuel_type": row["Fuel_Type"],
@@ -61,7 +61,7 @@ with open(csv_file, encoding="utf-8") as f:
         car_id = row['CarID']
         if car_id not in car:
             dealer_id = dealer[dealer_name]["dealer_id"]
-            model_id = manufacturer_models[manufacturer_model_key]["manucaturer_model_id"]
+            model_id = manufacturer_models[manufacturer_model_key]["manufacturer_model_id"]
             new_car = {
                 "car_id": uuid.uuid4(),
                 "mileage": int(row["Mileage"]),
@@ -71,22 +71,42 @@ with open(csv_file, encoding="utf-8") as f:
             }
             car[car_id] = new_car
 
+        car_feature_key = f'{car_id}|{feature_description}'
+        if car_feature_key not in car_features:
+            new_car_feature = {
+                "car_feature_id": uuid.uuid4(),
+                "car_id": car[car_id]["car_id"],
+                "feature_id": features[feature_description]["feature_id"]
+            }
+            car_features[car_feature_key] = new_car_feature
+
         accident_id = row["AccidentID"]
-        if accident_id not in accidents:
+        if accident_id not in accidents and accident_id not in (None or ""):
             new_accident = {
-                "accident_id": row["AccidentID"],
+                "accident_id": uuid.uuid4(),
                 "date_of_accident": row["Date_of_Accident"],
                 "description": row["Description"],
                 "repair_cost": row["Cost_of_Repair"],
-                "severity": str(row["Severity"]).upper(),
-                "car_id": car[car_id]["car_id"]   
+                "severity": str(row["Severity"]).capitalize(),
+                "car_id": car[car_id]["car_id"]
             }
             accidents[accident_id] = new_accident
-        # service_id = row["ServiceID"]
+        
+        service_id = row["ServiceID"]
+        if service_id not in services and service_id not in (None or ""):
+            new_service = {
+                "service_id": uuid.uuid4(),
+                "date_of_service": row["Date_of_Service"],
+                "service_type": row["ServiceType"],
+                "cost": row["Cost_of_Service"],
+                "car_id": car[car_id]["car_id"]   
+            }
+            services[service_id] = new_service
             
 
 
 def write_csv(filename, fieldnames, rows):
+
     with open(output_dir / filename, "w", newline='', encoding="utf-8") as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
@@ -94,7 +114,7 @@ def write_csv(filename, fieldnames, rows):
 
 write_csv("Manufacturers.csv", ["manufacturer_id", "name"], list(manufacturer.values()))
 write_csv("ManufacturerModels.csv", [
-    "manucaturer_model_id",
+    "manufacturer_model_id",
     "model_name",
     "engine_size",
     "fuel_type",
@@ -105,5 +125,5 @@ write_csv("Dealers.csv", ["dealer_id", "name", "city", "latitude", "longitude"],
 write_csv("Features.csv", ["feature_id", "description"], list(features.values()))
 write_csv("Cars.csv", ["car_id", "mileage", "price", "model_id", "dealer_id"], list(car.values()))
 write_csv("CarFeatures.csv", ["car_feature_id", "car_id", "feature_id"], list(car_features.values()))
-write_csv("Services.csv", ["service_id", "description", "date", "cost", "car_id"], list(services.values()))
+write_csv("Services.csv", ["service_id", "date_of_service", "service_type", "cost", "car_id"], list(services.values()))
 write_csv("Accidents.csv", ["accident_id", "date_of_accident", "description", "repair_cost", "severity", "car_id"], list(accidents.values()))
