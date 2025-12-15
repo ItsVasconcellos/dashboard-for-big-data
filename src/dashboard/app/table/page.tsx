fa845f6891f82a225cd8c536fe00d833d514c47a
ItsVasconcellos <fernando.asv@uol.com.br>
2025-12-15 09:24:10 +0000

    feat: table and data table

--- src/dashboard/app/table/page.tsx
+++ src/dashboard/app/table/page.tsx
@@ -1,3 +1,29 @@
+"use client"
+import TableComponent from "@/components/table";
+import { Tabs,TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { useEffect, useState } from "react";
+
 export default function Table(){
-    return(<></>)
+  const [activeTab, setActiveTab] = useState("dealers");
+
+  // useEffect(() => {
+  //     console.log("Active Tab:", activeTab);
+  //   }, [activeTab]);
+
+  return(
+    <div className="max-w-7xl mx-auto py-8">
+      <Tabs value={activeTab} onValueChange={setActiveTab}>
+        <TabsList>
+          <TabsTrigger value="dealers">Dealers</TabsTrigger>
+          <TabsTrigger value="vehicles">Vehicles</TabsTrigger>
+        </TabsList>
+        <TabsContent value="dealers">
+          <TableComponent columns={[]} data={[]}/>
+        </TabsContent>
+        <TabsContent value="vehicles">
+          <TableComponent columns={[]} data={[]}/>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
